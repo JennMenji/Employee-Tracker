ce35b016fd5d3cc6b387bd089920245a469bec80
Menji <jennifer.menjivar18@gmail.com>
2021-09-25 17:53:05 -0400

    initial files

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO departments (name)
+VALUES 
+    ('Sales'),
+    ('Finance'),
+    ('Security'),
+    ('Human Resources'),
+    ('IT');
+
+-- INSERT INTO roles (title, salary, department_id)
+-- VALUES 
+--     ('Manager', 90000),
+--     ('Accountant', 80000, 2),
+--     ('Officer', 78000, 3),
+--     ('Administrator', 65000, 4),
+--     ('Supervisor', 75000.0, 4),
+--     ('Technician', 85000.0, 5);
+
+-- INSERT INTO employees (first_name, last_name, role_id, manager_id)
+-- VALUES 
+--     ('James', 'Corben', 2),
+--     ('Jack', 'London', 4),
+--     ('Derek', 'Peets', 6, 3),
+--     ('Carline', 'Joseph', 1),
+--     ('Dennis', 'Cooper', 3),
+--     ('Monica', 'Bellucci', 5),
+--     ('Samuel', 'Johnson', 1),
+--     ('Tulse', 'Luper', 4, 6),
+--     ('Charles', 'Brown', 2),
+--     ('Eliza', 'Parsons', 6),
+--     ('Susan', 'Hill', 2),
+--     ('Sydney', 'Owenson', 3),
+--     ('Hubert', 'Crackanthorpe', 3),
+--     ('William', 'Carleton', 5),
+--     ('Gerald', 'Griffin', 4);
